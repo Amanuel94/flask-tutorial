be5706fbf14906e5b9fd932c6471309cc0f47b4d
Amanuel94 <amanueltewodros94@gmail.com>
2023-03-19 20:08:50 +0300

    created base.html

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ import os
 app =  Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
