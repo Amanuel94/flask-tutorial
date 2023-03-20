bc49389e0be336c5f7a99c60741b6dc8b8a39608
Amanuel94 <amanueltewodros94@gmail.com>
2023-03-20 12:46:44 +0300

    created routes.py

--- app.py
+++ app.py
@@ -3,14 +3,7 @@ import os
 
 app =  Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug =True)
